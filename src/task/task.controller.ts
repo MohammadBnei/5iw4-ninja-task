5a3a95196fb4796d9c93d6d14c25630e9d1337a8
jeremdh62 <jdhaille@myges.fr>
2023-03-12 20:50:29 +0100

    add function crud

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { TaskService } from './task.service';
-import { GrpcMethod } from '@nestjs/microservices';
+import { GrpcMethod, RpcException } from '@nestjs/microservices';
+import { CreateTaskDto } from './dto/create-task.dto';
 import {
   CreateTaskRequest,
   ListTasksRequest,
@@ -16,6 +17,28 @@ export class TaskController {
   @GrpcMethod('TaskService')
   createTask(data: CreateTaskRequest) {
     const newTask = data.task;
+
+    if (!newTask.title) {
+      throw new RpcException('Title is required');
+    } else if (typeof newTask.title !== 'string') {
+      throw new RpcException('Title must be a string');
+    }
+
+    if (!newTask.description) {
+      throw new RpcException('Description is required');
+    } else if (typeof newTask.description !== 'string') {
+      throw new RpcException('Description must be a string');
+    }
+
+    if (!newTask.status) {
+      throw new RpcException('Status is required');
+    } else if (
+      newTask.status !== Status.TODO &&
+      newTask.status !== Status.DOING &&
+      newTask.status !== Status.DONE
+    ) {
+      throw new RpcException('Status must be one of TODO, DOING, DONE');
+    }
 
     return this.taskService.create(newTask as any);
   }
@@ -36,5 +59,57 @@ export class TaskController {
     console.log({ res });
 
     return res;
+  }
+
+  @GrpcMethod('TaskService')
+  async GetTask(request: { id: number }): Promise<Task> {
+    if (!request.id) {
+      throw new RpcException('ID is required');
+    } else if (typeof request.id !== 'number') {
+      throw new RpcException('ID must be a number');
+    }
+
+    const task = await this.taskService.findById(request.id);
+
+    return Task.create({
+      title: task.title,
+    });
+  }
+
+  @GrpcMethod('TaskService')
+  async UpdateTask(request: {
+    id: number;
+    task: CreateTaskDto;
+  }): Promise<Task> {
+    if (!request.id) {
+      throw new RpcException('ID is required');
+    } else if (typeof request.id !== 'number') {
+      throw new RpcException('ID must be a number');
+    }
+
+    if (!request.task) {
+      throw new RpcException('Task is required');
+    } else if (typeof request.task !== 'object') {
+      throw new RpcException('Task must be an object');
+    }
+
+    const task = await this.taskService.update(request.id, request.task);
+
+    return Task.create({
+      title: task.title,
+    });
+  }
+
+  @GrpcMethod('TaskService')
+  async DeleteTask(request: { id: number }): Promise<void> {
+    if (!request.id) {
+      throw new RpcException('ID is required');
+    } else if (typeof request.id !== 'number') {
+      throw new RpcException('ID must be a number');
+    }
+
+    await this.taskService.remove(request.id);
+
+    return;
   }
 }
