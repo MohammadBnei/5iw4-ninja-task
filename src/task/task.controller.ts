a34b15983f49dd868f1d3b8eb06999b7bb1fd1ed
Flauz <dread94@gmail.com>
2023-03-14 14:07:46 +0100

    add crud

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,40 +1,122 @@
 import { Controller } from '@nestjs/common';
 import { TaskService } from './task.service';
-import { GrpcMethod } from '@nestjs/microservices';
+import { GrpcMethod, RpcException } from '@nestjs/microservices';
+import { CreateTaskDto } from './dto/create-task.dto';
 import {
   CreateTaskRequest,
+  GetTaskRequest,
   ListTasksRequest,
   ListTasksResponse,
   Status,
   Task,
+  UpdateTaskRequest,
 } from 'src/stubs/task/v1alpha/task';
+import { Status as OtherStatus } from '@prisma/client';
 
 @Controller()
 export class TaskController {
-  constructor(private readonly taskService: TaskService) {}
+  constructor(private readonly taskService: TaskService) { }
 
   @GrpcMethod('TaskService')
   createTask(data: CreateTaskRequest) {
-    const newTask = data.task;
-
-    return this.taskService.create(newTask as any);
+    if (!data.task) {
+      throw new RpcException({ code: 400, message: 'Task required' });
+    }
+    if (!(data.task.title && data.task.description)) {
+      throw new RpcException({ code: 400, message: 'Task requires title, description, dueDate' });
+    }
+    const t: CreateTaskDto = {
+      title: data.task.title,
+      description: data.task.description,
+      dueDate: data.task.dueDate ?? new Date().toISOString(),
+      status: OtherStatus.todo,
+    };
+    
+    return this.taskService.create(t);
   }
 
   @GrpcMethod('TaskService')
   async ListTasks(request: ListTasksRequest): Promise<ListTasksResponse> {
     const tasks = await this.taskService.findAll();
-    console.log({ tasks });
+    if (!tasks) {
+      throw new RpcException({ code: 404, message: 'No tasks found' });
 
+    }
     const res = ListTasksResponse.create({
       tasks: tasks.map((t) =>
         Task.create({
-          title: t.title,
-        }),
-      ),
+          id: t.id,
+          title: t.title
+        })
+      )
     });
-
-    console.log({ res });
-
     return res;
+  }
+
+  @GrpcMethod('TaskService')
+  async GetTask(request: GetTaskRequest): Promise<Task> {
+    if (!request.id)
+      throw new RpcException({ code: 400, message: 'Task id is required' });
+
+    const task = await this.taskService.findById(request.id);
+
+    if (!task)
+      throw new RpcException({ code: 404, message: "Task doesn't exist" });
+
+    const { title, description, dueDate, status } = task;
+
+    return Task.create({
+      title,
+      description,
+      dueDate: dueDate.toISOString(),
+      status: Status[status],
+    });
+  }
+
+  @GrpcMethod('TaskService')
+  async UpdateTask(request: UpdateTaskRequest): Promise<Task> {
+    if (!request.task)
+      throw new RpcException({ code: 400, message: 'Task is required' });
+
+    if (!(request.task.id && typeof request.task.id == 'number'))
+      throw new RpcException({ code: 400, message: 'Task id is required' });
+
+    const t = request.task;
+
+    if (t.status == Status.TODO)
+      t.status = OtherStatus.todo as any;
+    else if (t.status == Status.DOING)
+      t.status = OtherStatus.doing as any;
+    else if (t.status == Status.DONE)
+      t.status = OtherStatus.done as any;
+
+    const task = await this.taskService.update(t.id, t as any);
+
+    if (!task)
+      throw new RpcException({ code: 404, message: "Task doesn't exist" });
+
+    const { title, description, dueDate, status } = task;
+
+    return Task.create({
+      title,
+      description,
+      dueDate: dueDate.toISOString(),
+      status: Status[status],
+    });
+  }
+
+  @GrpcMethod('TaskService')
+  async DeleteTask(request: GetTaskRequest): Promise<any> {
+    if (!request.id)
+      throw new RpcException({ code: 400, message: 'Task requires id' });
+
+    const task = await this.taskService.findById(request.id);
+
+    if (!task)
+      throw new RpcException({ code: 404, message: "Task doesn't exist" });
+
+    await this.taskService.remove(request.id);
+
+    return { message: 'Task deleted' };
   }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -5,15 +5,17 @@ import {
 } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { PrismaService } from '../prisma/prisma.service';
+import { Status } from '@prisma/client';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
 export class TaskService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
-  create(createTaskDto: CreateTaskDto) {
-    return this.prisma.task.create({
-      data: createTaskDto,
-    });
+  create(task: CreateTaskDto) {
+    task.dueDate = new Date(task.dueDate);
+    task.status = Status.todo;
+    return this.prisma.task.create({ data: task });
   }
 
   findAll() {
@@ -22,23 +24,21 @@ export class TaskService {
 
   async findById(id: number) {
     const task = await this.prisma.task.findUnique({
-      where: { id },
+      where: { id }
     });
-    if (!task) {
-      throw new NotFoundException('Task not found');
-    }
-
+    if (!task)
+      throw new NotFoundException('No task found');
     return task;
   }
 
-  async update(id: number, data: CreateTaskDto) {
+  async update(id: number, data: UpdateTaskDto) {
     const task = await this.findById(id);
 
     try {
-      return this.prisma.task.update({
-        where: { id },
-        data,
-      });
+      if (data.dueDate) {
+        data.dueDate = new Date(data.dueDate);
+      }
+      return this.prisma.task.update({ where: { id }, data });
     } catch (error) {
       throw new Error(error.message);
     }
@@ -46,12 +46,10 @@ export class TaskService {
 
   async remove(id: number) {
     try {
-      return await this.prisma.task.delete({
-        where: { id },
-      });
+      return await this.prisma.task.delete({ where: { id } });
     } catch (error) {
       if (error?.code === 'P2025') {
-        throw new NotFoundException(`Task with id ${id} not found`);
+        throw new NotFoundException(`Unable to find task with id ${id}`);
       }
       throw new BadRequestException(error.message);
     }
